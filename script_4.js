dda052c6d7ae9620c45430b6275a5955231faa16
tommy-pellerin <tommytp@hotmail.fr>
2024-02-29 11:39:40 +0100

    startup nation ok

---
+++ script_4.js
@@ -0,0 +1,45 @@
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+//filtrer
+let seventies = []
+let reverseSeventies = []
+entrepreneurs.forEach(entrepreneur => {
+  let birthYear = entrepreneur.year
+  entrepreneur.age = 2024 - birthYear
+  console.log(entrepreneur)
+
+  if(birthYear >= 1970 && birthYear < 1980){
+    seventies.push(entrepreneur.first + " " + entrepreneur.last + " a " + entrepreneur.age);
+    reverseSeventies.push(entrepreneur.last + " " + entrepreneur.first)
+    
+  }  
+}  
+);
+//nom prenom et age
+
+// console.log(seventies);
+// for(let index in seventies){
+//   console.log(`Aujourd'hui ${seventies[index]}`)
+// }
+
+
+//trie par ordre alphabétique du nom de famille
+// console.log(reverseSeventies.sort());
